49c7b5e7ce7d2eec6e2070bd4e1e5c51c12670ec
Jtownokie <christopher.stephens@atlasschool.com>
2024-08-02 12:52:15 -0500

    Completed api with multi-parameter query implementation

--- api/index.js
+++ api/index.js
@@ -1,8 +1,34 @@
 // Defined Routes
+import Episode from '../utils/models/episode.js';
 
 // Routes Object
 const routes = (app) => {
   // GET Primary Query (Month, Subject(s), Color(s))
+  app.get('/episodes', async (req, res) => {
+    const { month, colors, subjects } = req.query;
+
+    try {
+      const query = {};
+
+      if (month) {
+        query.month = month;
+      }
+
+      if (colors) {
+        query.colors = { $all: colors.split(',') };
+      }
+
+      if (subjects) {
+        query.subjects = { $all: subjects.split(',') };
+      }
+
+      const episodes = await Episode.find(query);
+      res.status(200).send(episodes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: 'Data not found' });
+    }
+  });
 };
 
 // Export 
